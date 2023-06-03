64cf9f7136785472f8588fed698bd17dc65bcfe4
RAJ-KAMAL30 <113379882+RAJ-KAMAL30@users.noreply.github.com>
2023-06-03 08:52:34 +0000

    connect database and added data

--- app.py
+++ app.py
@@ -1,31 +1,15 @@
 from flask import Flask , render_template , request , flash , redirect
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine 
+from database import add_patient_to_database
 
 import os
 
-
-
 app=Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']=os.environ['DB_CONNECTION_STRING']
+app.secret_key = 'dont tell anyone'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DB_CONNECTION_STRING']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-database_connection_string = os.environ['DB_CONNECTION_STRING']
-
-engine = create_engine(database_connection_string,connect_args={
-        "ssl": {
-          "ssl_ca": "/etc/ssl/cert.pem"
-        }})
-
-db=SQLAlchemy(app)
-
-class Patient(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), nullable=False)
-    address = db.Column(db.String(100))
-    DOB = db.Column(db.String(20))
-    contact_info = db.Column(db.String(25))
 
 @app.route("/")
 def hello_home():
@@ -39,13 +23,14 @@ def submit_patient():
     DOB = request.form['DOB']
     contact_info = request.form['contact_info']
     
-    patient = Patient(name=name, address=address, DOB=DOB,contact_info=contact_info)     
+    add_patient_to_database(name, address, DOB, contact_info)
+  
 
-    db.session.add(patient)
-    db.session.commit()
+  return render_template('patients.html')  
 
-    flash('patient details added succesfully','success')
-    return redirect('/patients')
+    
+
+   
 
   return render_template('patients.html')
   
--- database.py
+++ database.py
@@ -1,20 +1,42 @@
-import sqlalchemy
-from sqlalchemy import create_engine , text
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, text 
+from flask import flash, redirect, render_template
+
 import os
 
-database_connection_string = os.environ['DB_CONNECTION_STRING']
+db_connection_string = os.environ["DB_CONNECTION_STRING"]
 
-engine = create_engine(database_connection_string,connect_args={
-        "ssl": {
-          "ssl_ca": "/etc/ssl/cert.pem"
-        }})
+engine = create_engine(db_connection_string, connect_args={
+  "ssl":{
+    "ssl_ca":"/etc/ssl/cert.pem"
+  }
+})
 
-def add_patient(name, address, dob, contact_info):
-  with engine.connect() as conn:
-    query = f"INSERT INTO patient(name, address, dob, contact_info) VALUES('{name}', '{address}', '{dob}','{contact_info}')"
-                   
-    conn.execute(query)
+
+def add_patient_to_database(name, address, DOB, contact_info):
+  with engine.connect() as conn :
+    
+    query = text("INSERT INTO patient(name, address, DOB,contact_info)VALUES(:name, :address, :DOB, :contact_info)")
+    conn.execute(query, {"name":name, "address":address, "DOB":DOB, "contact_info":contact_info})
+    flash('patient details added succesfully')
+    return render_template('patients.html') 
+    
+    
+    
+
+    
+    
+  
+
+
+
+
+
+  
+
+
+
+
+
 
   
     
